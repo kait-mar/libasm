bc56351bbc2196478243d66cc222f5321797d2d4
kait-mar <rim.bakkali63@gmail.com>
2020-11-12 14:59:57 +0100

    push

---
+++ hello_world.s
@@ -0,0 +1,26 @@
+;global  _main
+   ;extern  _printf
+
+    ;section .text
+;_main:
+  ;  push    message
+   ; call    _printf
+   ; add     esp, 4
+   ; ret
+;message:
+  ;  db      'Hello, World', 10, 0
+
+global start
+section .text
+start:
+  mov rax, 0x2000004 ; write
+  mov rdi, 1 ; stdout
+  mov rsi, msg
+  mov rdx, msg.len
+  syscall
+  mov rax, 0x2000001 ; exit
+  mov rdi, 0
+  syscall
+section .data
+msg:    db      "Hello, world!", 10
+.len:   equ     $ - msg
